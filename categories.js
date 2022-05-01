ccb14ddf06d113b58cb22c1daf678cecaca65c66
mhmdali102 <mhmdali102@bk.ru>
2022-05-01 05:38:04 +0300

    update dependencies

---
+++ categories.js
@@ -0,0 +1,12 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json(
+    { 
+      name: {
+        "1": "hi",
+        "2": "hello",
+      }
+    }
+  )
+}
