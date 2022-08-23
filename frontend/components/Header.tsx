140cd09ca7ff4715e6576fa1c71dd7c1630b7a26
mhmdali102 <mhmdali102@bk.ru>
2022-08-23 23:56:36 +0300

    ✅ hero section done

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = (props: {}) => {
+  return (
+    <header className="bg-black text-white">
+      <div className="container py-2">Header</div>
+    </header>
+  );
+};
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -10,12 +10,28 @@ const Home: NextPage = () => {
           name="description"
           content="We offer you ORIGINAL products from Canada in Lebanon!"
         />
-        <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#242424" />
       </Head>
 
-      <main className="container">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <main className="py-12">
+        <div className="container flex items-center justify-between flex-col">
+          <h2 className="text-3xl text-center font-bold max-w-[28ch] leading-relaxed">
+            We offer you{" "}
+            <span className="uppercase relative">
+              Original
+              <img
+                className="absolute bottom-0 left-0 right-0 w-full translate-y-1/2"
+                src="/images/underline.svg"
+                alt=""
+              />
+            </span>{" "}
+            Products from Canada in Lebanon!
+          </h2>
+          <div className="w-[70vw] max-w-[750px] relative my-16">
+            <img className="mx-auto mb-4" src="/images/lebanonTag.svg" alt="Lebanon pin tag" />
+            <img src="/images/hero.png" alt="Our Power tools brands" />
+          </div>
+        </div>
       </main>
     </div>
   );
