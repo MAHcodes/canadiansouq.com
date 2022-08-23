6ce111963e186cc771cb5984209100cb3b1c10d4
mhmdali102 <mhmdali102@bk.ru>
2022-08-23 23:12:21 +0300

    fonts, tailwindcss main done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {},
+    extend: {
+      colors: {
+        white: "rgb(230, 230, 230)",
+        fff: "rgb(255, 255, 255)",
+        gray: "rgb(145, 145, 145)",
+        black: "rgb(36, 36, 36)",
+        success: "rgb(39, 174, 96)",
+        danger: "rgb(233, 90, 90)",
+      },
+    },
   },
   plugins: [],
 };
