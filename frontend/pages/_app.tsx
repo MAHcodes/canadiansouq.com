0f01a79dd90f389e3fd96aaea29ee50f2cf8c90e
mhmdali102 <mhmdali102@bk.ru>
2022-08-29 06:26:10 +0300

    nprogress

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,6 +3,14 @@ import type { AppProps } from "next/app";
 import Header from "../components/Header";
 import React from "react";
 import { Footer } from "../components/Footer";
+import { Router } from "next/router";
+import NProgress from "nprogress";
+import "../styles/Nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+NProgress.configure({ showSpinner: false });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
