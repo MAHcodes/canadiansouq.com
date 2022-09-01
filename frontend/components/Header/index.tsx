9312c21feffdbc644de4642d50577b6b236907f9
mhmdali102 <mhmdali102@bk.ru>
2022-09-01 23:38:04 +0300

    header init

--- frontend/components/Header/index.tsx
+++ frontend/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface Props {
   categories: string[];
 }
@@ -5,12 +7,10 @@ interface Props {
 const Header = ({ categories }: Props) => {
   return (
     <header className="bg-black text-white rounded-b-lg sticky top-0 left-0 right-0 z-40">
-      <div className="container py-5">
-        <ul>
-          {categories.map((category: string) => (
-            <li key={category}>{category}</li>
-          ))}
-        </ul>
+      <div className="container py-4">
+        <Link href="/" passHref>
+          <h1 className="cursor-pointer font-bold 2xl:flex-1">Canadian Souq</h1>
+        </Link>
       </div>
     </header>
   );
