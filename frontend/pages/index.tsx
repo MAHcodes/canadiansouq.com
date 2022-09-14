a0a425d6512a4934a90368e91ebe7819a396b098
mhmdali102 <mhmdali102@bk.ru>
2022-09-14 10:06:12 +0300

    added favicon logo

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -21,6 +21,7 @@ const Home = ({ featuredProducts, newArrivalProcucts }: Props ) => {
           content="We offer you ORIGINAL products from Canada in Lebanon!"
         />
         <meta name="theme-color" content="#242424" />
+        <link rel="shortcut icon" href="/images/logo.svg" type="image/x-icon" />
       </Head>
 
       <Hero />
