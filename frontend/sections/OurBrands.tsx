56ad6b699d522ece3df80b4f271a86256404cb11
mhmdali102 <mhmdali102@bk.ru>
2022-11-17 09:19:42 +0200

    our brands tooltip

--- frontend/sections/OurBrands.tsx
+++ frontend/sections/OurBrands.tsx
@@ -16,19 +16,27 @@ const Brands = () => (
     <img
       src="/images/milwaukee-logo.svg"
       alt="Milwaukee Logo"
+      title="Milwaukee"
     />
-    <img className="brand-logo" src="/images/hilti-logo.svg" alt="HILTI Logo" />
+    <img
+      src="/images/hilti-logo.svg"
+      alt="HILTI Logo"
+      title="HILTI"
+    />
     <img
       src="/images/dewalt-logo.svg"
       alt="DeWALT Logo"
+      title="DeWALT"
     />
     <img
       src="/images/makita-logo.svg"
       alt="Makita Logo"
+      title="Makita"
     />
     <img
       src="/images/matcotools-logo.svg"
       alt="Matco Tools Logo"
+      title="Matco Tools"
     />
   </div>
 );
